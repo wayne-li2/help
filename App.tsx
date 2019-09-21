ba25dc7835316cad3dcc97825c43817e68c5ca7c
vicky11z <vicky11zhang@gmail.com>
2019-09-21 13:36:54 -0700

    screen setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListingScreen from './screens/ListingScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ListingScreen
+      address={'123 Main St'}
+    />
   );
 }
 
--- screens/ListingScreen.tsx
+++ screens/ListingScreen.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 
-export default class ListingReview extends React.Component {
+export type IProps = {
+  address: string;
+}
 
-  componentDidMount() {
-    await firebase
-      .database()
-      .ref('rental_units')
-      .once('value')
-      .then((data) => {
-        data.forEach((childNode) => {
-          var rental_unit = childNode.val();
-          rentalUnits[childNode.key] = {
-            address: rental_unit['address'],
-            average_rating: rental_unit['average_rating'],
-          };
-        });
-      });
-  }
-  constructor(props) {
-    super(props);
-    
+export default class ListingReview extends React.Component<IProps> {
+  
+  // componentDidMount() {
+  //   await firebase
+  //     .database()
+  //     .ref('rental_units')
+  //     .once('value')
+  //     .then((data) => {
+  //       data.forEach((childNode) => {
+  //         var rental_unit = childNode.val();
+  //         rentalUnits[childNode.key] = {
+  //           address: rental_unit['address'],
+  //           average_rating: rental_unit['average_rating'],
+  //         };
+  //       });
+  //     });
+  // }
+  render(): JSX.Element {
+    return (
+      <View>
+        <Text>{this.props.address}</Text>
+      </View>
+    )
   }
 }
